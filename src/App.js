584173ca876d381abf49bb6f64262fc2eb57725b
arushpandey2006 <ggtxarush@gmail.com>
2024-11-13 15:47:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Resetpassword from './component/Resetpassword';
+
+
+function App() {
+  return (
+   <Resetpassword />
+  );
+}
+
+export default App;
